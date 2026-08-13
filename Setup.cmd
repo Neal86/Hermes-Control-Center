f8054d7a642432ddcf87fdef81984e70ba4a22d9
Neal86 <52608308+Neal86@users.noreply.github.com>
2026-08-12 21:02:05 -0700

    Use non-blocking Setup V4 entrypoint

--- Setup.cmd
+++ Setup.cmd
@@ -1,7 +1,7 @@
 @echo off
 setlocal
 cd /d "%~dp0"
-powershell.exe -NoLogo -NoProfile -ExecutionPolicy Bypass -File "%~dp0Setup-Loop-v3.ps1"
+powershell.exe -NoLogo -NoProfile -ExecutionPolicy Bypass -File "%~dp0Setup-Loop-v4.ps1"
 set "EXITCODE=%ERRORLEVEL%"
 echo.
 if not "%EXITCODE%"=="0" (
