1d97f6104408f82f4556f0aac1dd0baafc27b1e4
Neal86 <52608308+Neal86@users.noreply.github.com>
2026-08-21 19:24:59 -0700

    refactor: add non-conflicting gateway domain package

---
+++ hcc_gateway/__init__.py
@@ -0,0 +1,5 @@
+"""Hermes Control Center gateway lifecycle/routing domain.
+
+This package is intentionally named hcc_gateway so it never shadows Hermes'
+own upstream `gateway` Python package.
+"""
